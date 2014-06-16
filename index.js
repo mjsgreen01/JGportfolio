baafc6266d6330ece018abe29dc33e65cf364b50
mjsgreen01 <mjsgreen01@gmail.com>
2014-06-16 09:48:51 -0400

    First Commit
    landing page and wrgw gallery

---
+++ index.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+
+	var amount = '';
+
+	function scrollForward() {
+	    if($('.designPreviewsContainer').scrollLeft()<'3455'){
+	    $('.designPreviewsContainer').animate({
+	        scrollLeft: amount
+	    }, 100, 'linear',function() {
+	        if (amount != '') {
+	            scrollForward();
+	        }
+	    });}
+	    else{$('.designPreviewsContainer').scrollLeft(0);
+	        scrollForward();}
+	}
+
+	function scrollBack() {
+	    
+	    if($('.designPreviewsContainer').scrollLeft()>'0'){
+	    $('.designPreviewsContainer').animate({
+	        scrollLeft: amount
+	    }, 100, 'linear',function() {
+	        if (amount != '') {
+	            scrollBack();
+	        }
+	    });}
+	    else{$('.designPreviewsContainer').scrollLeft(3455);
+	        scrollBack();}
+	}
+
+
+
+	$('.rightDesignArrow').hover(function() {
+	    amount = '+=30';
+	    scrollForward();
+	}, function() {
+	    amount = '';
+	});
+
+	$('.leftDesignArrow').hover(function() {
+	    amount = '-=30';
+	    scrollBack();
+	}, function() {
+	    amount = '';
+	});
+
+
+
+
+
+
+
+	$(".wrgwWrapper").on({click:function(){    
+    	if($(this).hasClass('wrgwWrapperClicked')){
+    		$(this).removeClass('wrgwWrapperClicked');
+    		$('.designPreviews').removeClass('designPreviewsClicked');
+    		$('.designPreviewsContainer').removeClass('designPreviewsContainerClicked');
+    		var scrolled = $('.designPreviewsContainer').scrollLeft()-125;
+			$('.designPreviewsContainer').scrollLeft(scrolled);
+    	}
+    	else{
+    		$(this).addClass('wrgwWrapperClicked');
+    		$('.designPreviews').addClass('designPreviewsClicked');
+    		$('.designPreviewsContainer').addClass('designPreviewsContainerClicked');
+    		var scrolled = $('.designPreviewsContainer').scrollLeft()+125;
+			$('.designPreviewsContainer').scrollLeft(scrolled);    		
+    	}
+
+		},
+		mouseleave: function(){
+			if($(this).hasClass('wrgwWrapperClicked')){
+	    		$(this).removeClass('wrgwWrapperClicked');
+	    		$('.designPreviews').removeClass('designPreviewsClicked');
+	    		$('.designPreviewsContainer').removeClass('designPreviewsContainerClicked');
+	    		var scrolled = $('.designPreviewsContainer').scrollLeft()-125;
+				$('.designPreviewsContainer').scrollLeft(scrolled);
+	    	}
+		}
+	});
+
+
+
+
+
+
+
+
+
+})
